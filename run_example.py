253f8aac5958b2e9d7a7684eab7820f1a100bc4d
TakeenJ <takeen.jazayeri@yahoo.com>
2023-07-26 15:00:38 +0330

    More examples

--- run_example.py
+++ run_example.py
@@ -1,14 +1,24 @@
 from run_code import Project
-import cv2
+import cv2, os
 import matplotlib.pyplot as plt
 
 
-proj = Project(image_address='Example', image_file='image.JPEG', model_name='cnn_model')
+for i in os.listdir('Example'):
+    if os.path.isdir(f'Example/{i}'):
+        image_file = None
+        for file in os.listdir(f'Example/{i}'):
+            if file.startswith("image"):
+                image_file = file
+        
+        if image_file == None:
+            print('NO IMAGE FILE FOUND')
 
-show_list = [proj.image, proj.enhanced_image, proj.ref_enhanced_image]
-for im in show_list:
-    if im.shape[2] == 3:
-        plt.imshow(cv2.cvtColor(im, cv2.COLOR_BGR2RGB))
-    elif im.shape[2] == 1:
-        plt.imshow(cv2.cvtColor(im, cv2.COLOR_GRAY2RGB))
-    plt.show()
+        proj = Project(image_address=f'Example/{i}', image_file=image_file, model_name='cnn_model')
+
+        show_list = [proj.image, proj.enhanced_image, proj.ref_enhanced_image]
+        for im in show_list:
+            if im.shape[2] == 3:
+                plt.imshow(cv2.cvtColor(im, cv2.COLOR_BGR2RGB))
+            elif im.shape[2] == 1:
+                plt.imshow(cv2.cvtColor(im, cv2.COLOR_GRAY2RGB))
+            plt.show()
